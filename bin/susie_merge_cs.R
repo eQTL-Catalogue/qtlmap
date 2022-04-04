391e8b5d93776ca55c3c98ae9d25d4f743a7edf9
kerimoff <kerimov.nurlan@gmail.com>
2022-04-04 11:04:25 +0300

    added log messages to susie_merge_cs.R script

--- bin/susie_merge_cs.R
+++ bin/susie_merge_cs.R
@@ -13,9 +13,10 @@ option_list <- list(
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
 
-#Import data
+message(" ## Reading credible sets.")
 credible_sets = read.table(opt$cs_results, header = TRUE, stringsAsFactors = FALSE, check.names = FALSE, sep = "\t") %>%
   dplyr::as_tibble()
+message(" ## Read ", nrow(credible_sets), " rows of credible sets.")
 
 if (nrow(credible_sets) == 0) {
   credible_sets = dplyr::tibble(
@@ -43,14 +44,17 @@ if (nrow(credible_sets) == 0) {
   quit(save = "no", status = 0)
 }
 
+message(" ## Reading summary statistics.")
 sumstats = read.table(opt$sumstats, header = TRUE, stringsAsFactors = FALSE, check.names = FALSE, sep = "\t") %>%
   dplyr::as_tibble() %>%
   dplyr::filter(molecular_trait_id %in% credible_sets$molecular_trait_id)
+message(" ## Read ", nrow(sumstats), " rows of summary statistics")
 
-#Merge the two tables
+message(" ## Merge the two tables")
 cs_table = dplyr::transmute(credible_sets, molecular_trait_id, variant, cs_id, pip, cs_size, z, cs_min_r2, finemapped_region) %>% 
   dplyr::left_join(sumstats, by = c("molecular_trait_id", "variant")) %>%
   dplyr::select(molecular_trait_id, gene_id, cs_id, variant, rsid, cs_size, pip, pvalue, beta, se, z, cs_min_r2, finemapped_region)
+message(" ## After merge there are ", nrow(sumstats), " rows of merged matrix")
 
 #Save file to disk
 file_handle = gzfile(opt$output,"w")
